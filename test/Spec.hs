39d2fc785f32d1c6effc241e8ddf665f20e99951
michaeljklein <michael14486@gmail.com>
2018-01-02 17:29:20 -0500

    add getRuns benchmarks

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,37 @@
+module Main where
+
+import Lib
+import Test.Lib
+import Test.Timers
+import Criterion.Main
+import Test.QuickCheck
+import Control.Monad
+import Foreign.C.Types
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain [
+        bgroup "getRuns" [
+          bench "getRuns  == getRunsA" $ nfIO (benchGetRuns getRuns  getRunsA),
+          bench "getRuns  == getRunsB" $ nfIO (benchGetRuns getRuns  getRunsB),
+          bench "getRunsA == getRunsB" $ nfIO (benchGetRuns getRunsA getRunsB)
+        ],
+        bgroup "getRuns'" [
+          bench "getRuns'  == getRuns'A" $ nfIO (benchGetRuns' getRuns'  getRuns'A),
+          bench "getRuns'  == getRuns'B" $ nfIO (benchGetRuns' getRuns'  getRuns'B),
+          bench "getRuns'A == getRuns'B" $ nfIO (benchGetRuns' getRuns'A getRuns'B)
+        ]
+       ]
+
+
+isSuccess :: Result -> Bool
+isSuccess (Success _ _ _) = True
+isSuccess  _              = False
+
+benchGetRuns :: ([ULL] -> [ULL]) -> ([ULL] -> [ULL]) -> IO Bool
+benchGetRuns x y = fmap isSuccess . quickCheckWithResult (stdArgs { chatty = False }) $ liftM2 (==) (x . fmap CULLong) (y . fmap CULLong)
+
+benchGetRuns' :: (ULL -> [ULL] -> [ULL]) -> (ULL -> [ULL] -> [ULL]) -> IO Bool
+benchGetRuns' x y = fmap isSuccess . quickCheckWithResult (stdArgs { chatty = False }) $ \z -> liftM2 (==) (x (CULLong z) . fmap CULLong) (y (CULLong z) . fmap CULLong)
+
+
---
+++ test/Test/Lib.hs
@@ -0,0 +1,2 @@
+module Test.Lib () where
+
