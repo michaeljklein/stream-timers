3f46d5c396c96c5df422872b844207a8e47b1cac
michaeljklein <michael14486@gmail.com>
2018-01-05 13:49:16 -0500

    added Expire monads for testing infinite loops

--- src/Timers.hs
+++ src/Timers.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Timers
     ( foldrOnce,
@@ -18,7 +21,13 @@ module Timers
       printEveryN,
       sideEffectEveryN,
       switchEvery',
-      switchEvery''
+      switchEvery'',
+      Expire(..),
+      ExpireIO(..),
+      getExpireIO,
+      dumpExpireIO,
+      killExpireIO,
+      ExpireE(..)
     ) where
 
 
@@ -26,6 +35,11 @@ import Control.Monad (foldM, liftM, liftM2)
 import Data.Time.Clock (UTCTime, getCurrentTime, utctDayTime, diffUTCTime)
 import Data.Void
 
+import Data.IORef
+import Control.Monad
+import System.Mem.Weak
+import Unsafe.Coerce
+import Control.Applicative
 
 -- reallyNoWay                             :: Eq a => (a -> a) -> a -> a
 -- reallyNoWay = (\f x -> if f x == x then x else fix f)
@@ -49,6 +63,152 @@ nestByM f x mn = foldM (\y z -> z f y) x =<< liftM (flip replicate ($)) mn
 -- | `nestMForever` is equivalent to `nestM f x Infinity`.
 nestMForever :: Monad m => (b -> m b) -> b -> m b
 nestMForever f x = foldM ((. const) . (.) $ f) x (repeat id)
+
+expireSteps :: Int
+expireSteps = 1
+
+newtype Expire a = Expire { getExpire :: (Int, Maybe a) } deriving (Eq, Ord, Show, Functor)
+
+instance Applicative Expire where
+  pure = Expire . (expireSteps,) . Just
+
+  (~(Expire (_, f))) <*> (~(Expire (xi, x))) = maybe (Expire (0, Nothing)) (\y -> Expire $! if xi <= 0 then (0, Nothing) else (xi - 1, Just y)) $! f <*> x
+                                         -- then Expire (0, Nothing)
+                                         -- else Expire (abs xi - 1, f <*> x)
+    -- where
+      -- i = xi
+  -- Expire (_ , _) <*> Expire (0 , _) = Expire (0, Nothing)
+  -- Expire (_ , f) <*> Expire (xi, x) = Expire (abs xi - 1, f <*> x)
+
+instance Monad Expire where
+  return = pure
+
+  (~(Expire (i, x))) >>= f = maybe (Expire (0, Nothing)) (((<*>) (Expire (i, flip const <$> x))) . f) x
+
+newtype ExpireE a = ExpireE { getExpireE :: (Int, a) } deriving (Eq, Ord, Show, Functor)
+
+instance Applicative ExpireE where
+  pure = ExpireE . (expireSteps,)
+
+  (~(ExpireE (fi, f))) <*> (~(ExpireE (xi, x))) = (\y -> ExpireE $! if i <= 0 then (0, error "ExpireE: ran out (<*>)") else (i - 1, y)) $! f x
+    where
+      i = min fi xi
+
+instance Monad ExpireE where
+  return = pure
+
+  (~(ExpireE (i, x))) >>= f = (((<*>) (ExpireE (i, flip const x))) . f) $! x
+
+
+newtype ExpireIO a = ExpireIO { runExpireIO :: IO (Weak (IORef (Int, Maybe a))) }
+
+-- mkWeakIORef :: IORef a -> IO () -> IO (Weak (IORef a))
+
+
+-- | Deref an `ExpireIO`, only returning the value, or return `Nothing`
+getExpireIO :: ExpireIO a -> IO (Maybe a)
+getExpireIO ~(ExpireIO wref) = do
+  wref' <- wref
+  ref   <- deRefWeak wref'
+  maybe (return Nothing) (fmap snd . readIORef) ref
+
+-- | Dereference an `ExpireIO`, or return `Nothing`
+dumpExpireIO :: ExpireIO a -> IO (Maybe (Int, a))
+dumpExpireIO ~(ExpireIO wref) = do
+  wref' <- wref
+  ref   <- deRefWeak wref'
+  maybe (return Nothing) ((>>= \(i, x) -> return ((i,) <$> x)) . readIORef) ref
+
+-- | `finalize` an `ExpireIO`
+killExpireIO :: ExpireIO a -> IO ()
+killExpireIO ~(ExpireIO wref) = do
+  wref' <- wref
+  finalize wref'
+
+
+-- | Note: `stepExpire` will never halt if `Int` is negative, so instead it throws an `error`
+stepExpire :: Int -> a -> (Int, Maybe a)
+stepExpire 0 _             = (0, Nothing)
+stepExpire 1 _             = (0, Nothing)
+stepExpire i x | i < 0     = error "stepExpire: negative input, i.e. caught infinite loop"
+               | otherwise = (i-1, Just x)
+
+-- | `stepExpire` for `Maybe`
+stepExpireMaybe :: Int -> Maybe a -> (Int, Maybe a)
+stepExpireMaybe _ Nothing   = (0, Nothing)
+stepExpireMaybe i ~(Just x) = stepExpire i x
+
+-- | Note: This is only safe if a `Weak` pointer has _already_ been dereferenced
+coerceWeak :: Weak a -> Weak b
+coerceWeak = unsafeCoerce
+
+
+instance Functor ExpireIO where
+  fmap :: (a -> b) -> ExpireIO a -> ExpireIO b
+  fmap f (ExpireIO wref) = ExpireIO $ do
+    wref' <- wref
+    ref   <- deRefWeak wref'
+    flip (maybe (unsafeCoerce wref)) ref $ \ioRef -> do
+      (i, x) <- fmap (fmap f) . uncurry stepExpireMaybe <$> readIORef ioRef
+      case x of
+        Nothing    -> do
+          finalize wref'
+          fmap unsafeCoerce wref -- deRefWeak should now result in null
+        ~(Just x') -> do
+          finalize wref'
+          ioRef' <- newIORef (i, Just x')
+          mkWeakIORef ioRef' (return ())
+
+instance Applicative ExpireIO where
+  pure x = ExpireIO $ do
+    ioRef <- newIORef (expireSteps, Just x)
+    mkWeakIORef ioRef (return ())
+
+  ExpireIO fWref <*> ExpireIO xWref = ExpireIO $ do
+    fWref' <- fWref
+    xWref' <- xWref
+    fRef' <- deRefWeak fWref'
+    xRef' <- deRefWeak xWref'
+    case liftM2 (,) fRef' xRef' of
+      Nothing -> do -- either input was unable to be dereferenced, ensure both are finalized and do nothing
+        finalize fWref'
+        finalize xWref'
+        return $ coerceWeak xWref'
+      ~(Just (fIOref, xIOref)) -> do -- both were successfully dereferenced!
+        (i, x) <- uncurry stepExpireMaybe <$> liftM2 (\(_, b) (c, d) -> (c, b <*> d)) (readIORef fIOref) (readIORef xIOref)
+        case x of
+          Nothing -> do -- just ended, finalize both
+            finalize fWref'
+            finalize xWref'
+            return $ coerceWeak xWref'
+          ~(Just y) -> do -- make new value, finalizing x
+            finalize fWref'
+            yRef <- newIORef (i, Just y)
+            mkWeakIORef yRef (return ())
+
+instance Monad ExpireIO where
+  return = pure
+
+  ExpireIO wref >>= f = ExpireIO $ do
+    wref' <- wref
+    ref   <- deRefWeak wref'
+    case ref of
+      Nothing -> do
+        finalize wref'
+        return $ coerceWeak wref'
+      ~(Just ioRef) -> do
+        ~(_, x) <- uncurry stepExpireMaybe <$> readIORef ioRef
+        case x of
+          Nothing -> do
+            finalize wref'
+            return $ coerceWeak wref'
+          ~(Just y) -> do
+            finalize wref'
+            runExpireIO $ (flip const <$> ExpireIO wref) <*> f y
+
+
+-- Hopefully, ExpireIO will give near-instant expiration, not like Expire, which seems to take around 1ns per action after expiration.
+
 
 -- | `foldrOnce` applies a right fold, only once (for streams, primarily).
 foldrOnce :: (a -> b -> a) -> (a, [b]) -> (a, [b])
