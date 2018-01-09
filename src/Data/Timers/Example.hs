0b2073780c1e2cc0bead79a1ec4cb938cb82ba4c
michaeljklein <michael14486@gmail.com>
2018-01-08 20:36:26 -0500

    added fixed-vector, cleaned up a bunch of comments, added some comments, compiling

--- src/Data/Timers/Example.hs
+++ src/Data/Timers/Example.hs
@@ -1,4 +1,9 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Data.Timers.Example
     ( Map,
@@ -8,8 +13,8 @@ module Data.Timers.Example
       countGapless,
       countGapless',
       nextGapless,
-      onlyGapless,
       toMathematica,
+      onlyGapless,
       onlyGaplessMathematica,
       differences,
       differences_,
@@ -21,21 +26,32 @@ module Data.Timers.Example
       getRuns'A,
       getRuns'B,
       addPosition,
+      Vec2,
+      Vec4,
+      Vec5,
+      addPosition',
       theStream,
       printMap,
+      printMap',
       printEveryNOfStream,
-      returnEveryN
+      printEveryNOfStream',
+      returnEveryN,
+      returnEveryN'
     ) where
 
 
 import Data.Bits (Bits, (.&.), unsafeShiftR)
--- import Foreign.C.Types (CULLong)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Timers (foldlOnce, sideEffectEveryN, printEveryN) -- foldrTimes,
 import Control.Monad (liftM2)
 import Data.Map.Internal.Debug (showTreeWith)
 import Data.List (group)
+
+import Data.Vector.Fixed.Cont (ContVec, mk5)
+import Data.Vector.Fixed ()
+import qualified Data.Vector.Fixed as FV
+
 
 
 -- | Mystery function... (read the source)
@@ -91,13 +107,15 @@ nextGapless = until gapless (+1)
 onlyGapless :: (Bits t, Num t) => [t] -> [t]
 onlyGapless = filter gapless
 
+-- | `onlyGapless`, differences, grouped, then converted to Mathematica lists.
 -- {-# SPECIALIZE onlyGaplessMathematica ::         [CULLong] -> [Char] #-}
 onlyGaplessMathematica :: (Bits t, Num t, Show t) => [t] -> String
 onlyGaplessMathematica = toMathematica . group . differences . onlyGapless
 
+-- | Convert a list of lists to a list of Mathematica lists.
 -- {-# SPECIALIZE toMathematica ::         [[CULLong]] -> [Char] #-}
-toMathematica :: (Show a, Foldable t) => t [a] -> String
-toMathematica = concatMap (\x->concat["{",show(head x),",",show(length x),"},"])
+toMathematica :: Show a => [[a]] -> String
+toMathematica = (>>= (\x->concat["{",show(head x),",",show(length x),"},"]))
 
 
 -- | First differences:
@@ -113,6 +131,7 @@ differences = zipWith (-) =<< tail
 differences_ :: Num a => [a] -> [a]
 differences_ = (\x->zipWith(-)(tail x)x)
 
+-- | Compare `differences` and `differences_`, specialized for testing
 prop_differences_ :: [Int] -> Bool
 prop_differences_ = liftM2 (==) differences differences_
 
@@ -164,17 +183,25 @@ getRuns'B n (_:xs) =     getRuns'B (n+1) xs
 getRuns'B _  _     = []
 
 
--- -- | More readable (and specialized) than @zip [0..]@
--- {-# SPECIALIZE notePosition :: [CULLong] -> [(CULLong, CULLong)] #-}
--- notePosition :: (Enum a, Num a) => [b] -> [(a, b)]
--- notePosition = zip [0..]
-
-
 -- addPosition :: (CULLong, CULLong)                                              -- ^ (position, value)
 --             -> Map CULLong ((CULLong,CULLong,CULLong,CULLong,CULLong),[(CULLong,CULLong,CULLong,CULLong,CULLong)]) -- ^ Map Value (last input position, list of its previous runs)
 --             -> Map CULLong ((CULLong,CULLong,CULLong,CULLong,CULLong),[(CULLong,CULLong,CULLong,CULLong,CULLong)]) -- ^ Map Value (new  input position, list of its previous runs)
-addPosition :: (Ord k0, Eq a, Num a) => (a, k0) -> Map k0 ((a, a, a, a, a), [(a, a, a, a, a)]) -> Map k0 ((a, a, a, a, a), [(a, a, a, a, a)])
+addPosition :: (Ord k0, Eq a, Num a) => (a, k0)
+                                     -> Map k0 ((a, a, a, a, a), [(a, a, a, a, a)])
+                                     -> Map k0 ((a, a, a, a, a), [(a, a, a, a, a)])
 addPosition (position, value) posMap = Map.insertWith posValInsert value ((position,0,0,0,0),[]) posMap
+
+
+
+type Vec2 = ContVec 2
+type Vec4 = ContVec 4
+type Vec5 = ContVec 5
+
+
+-- | A version of `addPosition` using `Vec5` instead of tuples
+addPosition' :: (Ord k0, Eq a, Num a) => a -> k0 -> Map k0 (Vec5 a, [Vec5 a]) -> Map k0 (Vec5 a, [Vec5 a])
+addPosition' position value posMap = Map.insertWith posValInsert' value (mk5 position 0 0 0 0, []) posMap
+
 
 -- Some code, I believe an alternate attempt/pseudocode/practice
 -- @
@@ -186,8 +213,8 @@ addPosition (position, value) posMap = Map.insertWith posValInsert value ((posit
 --     positions = (     pos2,pos3,pos4,pos5)
 -- @
 posValInsert :: (Eq a, Num a) => ((a,a,a,a,a),[(a,a,a,a,a)]) ->
-                         ((a,a,a,a,a),[(a,a,a,a,a)]) ->
-                         ((a,a,a,a,a),[(a,a,a,a,a)])
+                                 ((a,a,a,a,a),[(a,a,a,a,a)]) ->
+                                 ((a,a,a,a,a),[(a,a,a,a,a)])
 
 posValInsert ((pos6,_,_,_,_),_) ((pos1,pos2,pos3,pos4,pos5), pastRuns) = if newRun `elem` pastRuns || 0 `elem` [pos1,pos2,pos3,pos4,pos5,pos6]
                                                                             then (positions,          pastRuns)
@@ -200,11 +227,28 @@ posValInsert ((pos6,_,_,_,_),_) ((pos1,pos2,pos3,pos4,pos5), pastRuns) = if newR
     positions = (pos2     ,pos3     ,pos4     ,pos5     ,pos6     )
 
 
+-- | A version of `posValInsert` using `Vec4` and `Vec5` instead of tuples
+posValInsert' :: (Eq a, Num a) => (Vec5 a, [Vec5 a]) -> (Vec5 a, [Vec5 a]) -> (Vec5 a, [Vec5 a])
+posValInsert' ~((x :: Vec5 a), _) ~(y, zs) = if any (FV.and . FV.zipWith (==) newRun) zs || 0 == sixth || 0 `elem` y
+                                     then (positions,          zs)
+                                     else (positions, newRun : zs)
+  where
+    sixth     = FV.head x
+    temp      = (FV.tail :: Vec5 a -> Vec4 a) y -- Vec4
+    positions = FV.snoc sixth temp -- Vec5
+    newRun    = FV.zipWith (-) positions y -- Vec5
 
 
 
+
+
+-- | Print the resulting `Map`
 printMap :: forall a1 a b c. (Show b, Show c) => (a1, Map b (a, [(c, c, c, c, c)])) -> IO ()
 printMap = putStrLn . showTreeWith (\k x -> show (k, snd x)) True False . snd
+
+-- | Print the resulting `Map`, specialized to `Vec5`'s instead of tuples
+printMap' :: (Show a0, Show a2) => (a, Map a0 (a1, [Vec5 a2])) -> IO ()
+printMap' = (putStrLn . showTreeWith (\k ~(_, xs) -> show (k, FV.toList <$> xs)) True False . snd)
 
 
 ----- Ahhhhh need differences in positions of differences..
@@ -213,23 +257,37 @@ printMap = putStrLn . showTreeWith (\k x -> show (k, snd x)) True False . snd
 --  ^ this is what to extract positions of 1s, 2s.. and then take the differences of those positions and store those runs...
 
 
-
+-- | The initial state of the stream
 -- {-# SPECIALIZE theStream :: (Bits a1, Num a1, Enum a1) => ([(CULLong, CULLong)], Map a1 a1) #-}
--- theStream :: (Num w, Enum w, Bits w, Bits a, Num a, Enum a) => ([(w, w)], Map a a)
--- theStream :: (Num k, Bits k, Enum k, Num a, Bits a, Enum a) => ([(CULLong, CULLong)], Map k a)
--- theStream = baseMap . notePosition . getRuns . differences . onlyGapless $ [1..]
-theStream :: (Bits a1, Num a1, Enum a1) => ([(w, w)], Map a1 a1)
-theStream = error "stopgap"
---   where
---     -- baseMap :: a -> (a, Map k' a1')
---     baseMap = flip (,) Map.empty
+theStream :: (Enum a, Enum b, Num b, Bits b, Num a) => ([(a, b)], Map k v)
+theStream = (, Map.empty) . zip [1..] . lock getRuns . differences . lock onlyGapless $ [1..]
+  where
+    lock :: ([t] -> [t]) -> [t] -> [t]
+    lock = id
 
+-- | Print every nth value of the stream, specialized to the following value (it's an infinite loop, so the return value simply encodes which types the algorithm is specialized to).
+-- {-# SPECIALIZE printEveryNOfStream :: Int -> IO ([(CULLong, CULLong)], Map CULLong ((CULLong, CULLong, CULLong, CULLong, CULLong), [(CULLong, CULLong, CULLong, CULLong, CULLong)])) #-}
+printEveryNOfStream :: (Eq a, Bits k0, Num k0, Num a, Enum k0, Enum a, Ord k0, Show k0, Show a) =>
+     Int -> IO ([(a, k0)], Map k0 ((a, a, a, a, a), [(a, a, a, a, a)]))
+printEveryNOfStream n = printEveryN (foldlOnce addPosition) n theStream
 
--- printEveryNOfStream :: Int -> IO ([(CULLong, CULLong)], Map CULLong ((CULLong, CULLong, CULLong, CULLong, CULLong), [(CULLong, CULLong, CULLong, CULLong, CULLong)]))
-printEveryNOfStream n = error "stopgep" -- = printEveryN (foldlOnce addPosition) n theStream
+-- | `printEveryNOfStream`, secialized to `ContVec`'s and using `addPosition'`
+printEveryNOfStream' :: (Eq a, Bits k0, Num k0, Num a, Enum k0, Enum a, Ord k0, Show (ContVec 5 a), Show k0, Show a) =>
+     Int -> IO ([(a, k0)], Map k0 (Vec5 a, [Vec5 a]))
+printEveryNOfStream' n = printEveryN (foldlOnce (uncurry addPosition')) n theStream
+
 
 -- returnEveryN :: Int -> IO ([(CULLong, CULLong)], Map CULLong ((CULLong, CULLong, CULLong, CULLong, CULLong), [(CULLong, CULLong, CULLong, CULLong, CULLong)]))
-returnEveryN n = error "stopgep" -- sideEffectEveryN printMap (foldlOnce addPosition) n theStream
+-- | See `printEveryNOfStream`, except is uses `sideEffectEveryN` instead of `printEveryN`
+returnEveryN :: (Eq c, Show b, Show c, Ord b, Enum c, Enum b, Num c, Num b, Bits b) => Int -- ^ How often
+             -> IO ([(c, b)], Map b ((c, c, c, c, c), [(c, c, c, c, c)])) -- ^ Results from `theStream`
+returnEveryN n = sideEffectEveryN printMap (foldlOnce addPosition) n theStream
+
+-- | A version of `returnEveryN` using `addPosition'` and `printMap'`
+returnEveryN' :: (Eq a2, Enum a2, Num a2, Bits a1, Enum a1, Num a1, Ord a1, Show a2, Show a1) => Int  -- ^ How often
+              -> IO ([(a2, a1)], Map a1 (Vec5 a2, [ContVec 5 a2])) -- ^ Results from `theStream`
+returnEveryN' n = sideEffectEveryN printMap' (foldlOnce (uncurry addPosition')) n theStream
+
 
 
 -- get runs of 1's, 4^n's
